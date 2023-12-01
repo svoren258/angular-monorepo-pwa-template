5ca2f9ff525654f07d6ce987a31f38493a20062c
svoren258 <svoren258@gmail.com>
2023-12-01 21:20:43 +0100

    chore(package.json): custom material theme used

--- apps/angular-monorepo-pwa-template/src/app/app.component.ts
+++ apps/angular-monorepo-pwa-template/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, DashboardComponent],
   selector: 'angular-monorepo-pwa-template-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
