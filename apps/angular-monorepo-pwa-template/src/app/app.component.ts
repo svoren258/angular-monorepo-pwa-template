6fb6dcba1f7bf75c237e5693d9186e90506e3e31
svoren258 <svoren258@gmail.com>
2023-12-02 20:54:09 +0100

    refactor(app.component): nx welcome component removed

--- apps/angular-monorepo-pwa-template/src/app/app.component.ts
+++ apps/angular-monorepo-pwa-template/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, DashboardComponent],
+  imports: [RouterModule, DashboardComponent],
   selector: 'angular-monorepo-pwa-template-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
