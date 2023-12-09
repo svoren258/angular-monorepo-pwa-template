959aae697257a01af0a432cf81d5622ddcf40475
svoren258 <svoren258@gmail.com>
2023-12-09 19:59:42 +0100

    refactor(home-ui): card component layout adjusted

--- libs/home-ui/src/lib/home-ui/home-ui.component.ts
+++ libs/home-ui/src/lib/home-ui/home-ui.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { CardFirestoreService } from '@my-pwa/home-data-access';
@@ -8,7 +9,7 @@ import { CardComponent } from '../card/card.component';
 @Component({
   selector: 'my-pwa-template-home-ui',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, MatCardModule, CardComponent],
+  imports: [CommonModule, MatGridListModule, MatCardModule, CardComponent, MatButtonModule],
   templateUrl: './home-ui.component.html',
   styleUrl: './home-ui.component.css',
 })
