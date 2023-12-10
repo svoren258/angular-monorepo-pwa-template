94d403a3b7d9762a1d6c89c42ce777aa388361ef
svoren258 <svoren258@gmail.com>
2023-12-10 20:28:39 +0100

    refactor(home-ui): sidenav redesigned

--- libs/shared-ui/src/lib/sidenav/sidenav.component.ts
+++ libs/shared-ui/src/lib/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterOutlet } from '@angular/router';
@@ -10,7 +11,8 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     MatListModule,
     MatIconModule,
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    MatButtonModule
   ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
